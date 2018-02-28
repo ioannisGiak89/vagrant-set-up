656c64033d19b8e3aeb0fccf60ba5cf6db38d4d1
Ioannis Giakoumidis <ioannis.giakoumidis@gmail.com>
2018-02-28 12:17:26 +0000

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -2,10 +2,7 @@
 
 echo "Install varius projects"
 
-sudo echo "192.168.33.10 atlas
-192.168.33.10 gracire
-192.168.33.10 redwood
-192.168.33.10 schuss
-192.168.33.10 mauritius" >> /etc/hosts
+sudo echo "192.168.33.10 my_project_name
+192.168.33.10 my_project_name_2" >> /etc/hosts
 
 vagrant up
